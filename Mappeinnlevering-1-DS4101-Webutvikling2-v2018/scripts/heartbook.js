0959d7743e53d2934f285e3b4730bac85854f037
Haugens <larsmagnushaugen@gmail.com>
2018-03-02 13:34:19 +0100

    heartbook.js

--- Mappeinnlevering-1-DS4101-Webutvikling2-v2018/scripts/heartbook.js
+++ Mappeinnlevering-1-DS4101-Webutvikling2-v2018/scripts/heartbook.js
@@ -1,20 +1,36 @@
 $(function(){
-    
+
+    let profileImg = $("<img>");
+    let profileName = $("<h1>");
+    let profileAge = $("<p>");
+    let profileGender = $("<p>");
+    let profileInterests = $("<p>");
+
     
     $("showAllBtn").click(function(){
         
+        $.each()(function(){
+
+        })
+
+        $("profil").append(profileImg, profileName, profileAge, profileGender, profileInterests);
     });
 
     $("showAllMenBtn").click(function(){
-        
+        $("profil").append(profileImg, profileName, profileAge, profileGender, profileInterests);
     });
 
     $("showAllWomenBtn").click(function(){
-        
+        $("profil").append(profileImg, profileName, profileAge, profileGender, profileInterests);
     });
 
     $("iFeelLuckyBtn").click(function(){
         
+        $("profil").append(profileImg, profileName, profileAge, profileGender, profileInterests);
     });
 
 })
+
+
+//css
+

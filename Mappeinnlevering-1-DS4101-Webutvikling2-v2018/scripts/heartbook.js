cfb878f4ef46e39d7f52bd3d20f18d26d5d719b8
Haugens <larsmagnushaugen@gmail.com>
2018-03-02 12:54:16 +0100

    lagt til knapper i js

---
+++ Mappeinnlevering-1-DS4101-Webutvikling2-v2018/scripts/heartbook.js
@@ -0,0 +1,20 @@
+$(function(){
+    
+    
+    $("showAllBtn").click(function(){
+        
+    });
+
+    $("showAllMenBtn").click(function(){
+        
+    });
+
+    $("showAllWomenBtn").click(function(){
+        
+    });
+
+    $("iFeelLuckyBtn").click(function(){
+        
+    });
+
+})

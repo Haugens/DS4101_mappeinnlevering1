466f70034fc289bf6fee76d9e1bbb46923d22fa6
Phylis Ama D Boateng-G <boaphy16@student.westerdals.no>
2018-03-04 20:33:24 +0100

    add

---
+++ Mappeinnlevering-1-DS4101-Webutvikling2-v2018/scripts/init-heartbook.js
@@ -0,0 +1,6 @@
+$(function(){
+    
+    // id i section/container = "random-kung-fu-section"
+    $("#profil").html(HeartbookApp.init());
+    
+});
--- Mappeinnlevering-1-DS4101-Webutvikling2-v2018/scripts/modules/HeartbookProfileModule.js
+++ Mappeinnlevering-1-DS4101-Webutvikling2-v2018/scripts/modules/HeartbookProfileModule.js
@@ -1,53 +1,69 @@
-const profilesJSON = [{
+const HeartbookProfileModule = (function () {
+
+    const profilesJSON = [
+        {
             "firstName": "Arne",
             "age": "32",
             "imageSrc": "arne.jpg",
             "description": "",
             "gender": "Male"
-        }, {
+        },
+        {
             "name": "Filip",
             "age": "29",
             "imageSrc": "filip.jpg",
             "description": "",
             "gender": "Male"
-        }, {
+        },
+        {
             "name": "Elise",
             "age": "32",
             "imageSrc": "elise.jpg",
             "description": "",
             "gender": "Female"
-        }, {
+        },
+        {
             "name": "Ina",
             "age": "26",
             "imageSrc": "ina.jpg",
             "description": "",
             "gender": "Female"
-        }, {
+        }, 
+        {
             "name": "Mark",
             "age": "33",
             "imageSrc": "mark.jpg",
             "description": "",
             "gender": "Male"
-        }, {
+        }, 
+        {
             "name": "Peter",
             "age": "29",
             "imageSrc": "peter.jpg",
             "description": "",
             "gender": "Male"
-        }, {
-            "name": "Randi", 
+        }, 
+        {
+            "name": "Randi",
             "age": "34",
             "imageSrc": "randi.jpg",
             "description": "",
             "gender": "Female"
-        }, {
+        }, 
+        {
             "name": "Tone",
             "age": "27",
             "imageSrc": "tone.jpg",
             "description": "",
             "gender": "Female"
-        }];
-
-        return{
-            profilesJSON
         }
+    ];
+
+    return {
+        profilesJSON
+    }
+    
+    
+    get
+
+}())
